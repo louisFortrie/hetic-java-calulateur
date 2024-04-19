333b5c65705e62385e25679f0c74a579705d1703
louis fortrie <louis.fortrie4@gmail.com>
2024-04-19 13:30:14 +0200

    feat: adding a beans.xml to instantiate the reader :sparkles:

--- src/main/java/fr/hetic/Calculateur.java
+++ src/main/java/fr/hetic/Calculateur.java
@@ -2,8 +2,9 @@ package fr.hetic;
 
 import fr.hetic.readers.*;
 
-import java.util.Properties;
-import java.io.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 class LineProcessingException extends Exception {
     public LineProcessingException(String message) {
@@ -13,35 +14,15 @@ class LineProcessingException extends Exception {
 
 public class Calculateur {
     public static void main(String[] args) throws LineProcessingException {
-        Properties properties = new Properties();
-        try (InputStream input = Calculateur.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                System.out.println("Sorry, unable to find application.properties");
-                return;
-            }
-
-            properties.load(input);
-            String type = properties.getProperty("reader.type").trim();
-            DataReader dataReader;
-          
-            if ("JDBC".equals(type)) {
-                dataReader = new JDBCDataReader();
-            } else if ("FILE".equals(type)) {
-                if (args.length != 1) {
-                    System.out.println("Expected : java -jar Exercice7.jar <inputName | directoryName>");
-                    return;
-                }
-                String inputDirectoryPath = args[0];
-                dataReader = new FileDataReader(inputDirectoryPath);
-            } else {
-                System.out.println("Invalid type specified " + type);
-                return;
-            }
+           try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+            DataReader dataReader = (DataReader) context.getBean("dataReader");
 
             dataReader.process();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
         }
     }
+    }
    
-}
